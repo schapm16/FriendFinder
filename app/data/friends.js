836b9cbb50430c2f6c711f10bc01846a7331f793
Stephen Chapman <schapm16@gmail.com>
2017-12-11 23:28:23 +0000

    Fill in friends.js data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,82 +1,82 @@
 var friends = [
   {
-    "name": "",
-    "photo": "",
+    "name": "Bert",
+    "photo": "https://bfmbrainfall.files.wordpress.com/2016/03/are_you_ernie_or_bert.jpg?w=359",
     "scores": [
-      1,
-      1,
-      1,
-      1,
-      1,
-      1,
-      1,
-      8,
-      9,
-      10
+      3,
+      2,
+      3,
+      4,
+      5,
+      5,
+      3,
+      2,
+      5,
+      4
     ]
   },
   {
-    "name": "",
-    "photo": "",
+    "name": "Ernie",
+    "photo": "https://vignette.wikia.nocookie.net/muppet/images/2/2e/ErnieFullFigure-NEW.jpg/revision/latest?cb=20100901224144",
     "scores": [
+      4,
+      5,
+      4,
       1,
-      1,
-      1,
-      1,
-      1,
-      1,
-      1,
-      8,
-      9,
-      10
+      2,
+      2,
+      5,
+      4,
+      2,
+      5
     ]
   },
   {
-    "name": "",
-    "photo": "",
+    "name": "Grouch",
+    "photo": "https://vignette.wikia.nocookie.net/muppet/images/5/5f/OscarsClassicStare.jpg/revision/latest?cb=20120222062511",
     "scores": [
+      5,
       1,
       1,
+      5,
+      2,
+      2,
       1,
       1,
-      1,
-      1,
-      1,
-      8,
-      9,
-      10
+      5,
+      1
     ]
   },
   {
-    "name": "",
-    "photo": "",
+    "name": "Mr. Snuffleupagus",
+    "photo": "https://vignette.wikia.nocookie.net/muppet/images/f/fc/Character.snuffy.jpg/revision/latest?cb=20100405201116",
     "scores": [
-      1,
-      1,
-      1,
-      1,
-      1,
-      1,
-      1,
-      8,
-      9,
-      10
+      2,
+      3,
+      5,
+      4,
+      4,
+      2,
+      3,
+      3,
+      4,
+      4
     ]
   },
   {
-    "name": "",
-    "photo": "",
+    "name": "Elmo",
+    "photo": "https://vignette.wikia.nocookie.net/muppet/images/5/5a/Elmo-elmo-elmo.jpg/revision/latest?cb=20110917000614",
     "scores": [
+      5,
+      5,
+      5,
       1,
-      1,
-      1,
-      1,
-      1,
-      1,
-      1,
-      8,
-      9,
-      10
+      5,
+      5,
+      5,
+      5,
+      3,
+      5
     ]
   }
 
